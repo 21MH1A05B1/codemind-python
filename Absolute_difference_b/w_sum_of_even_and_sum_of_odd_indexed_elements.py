da40ef3668df70b1de98c758ffa2de677135fd82
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-05-28 15:24:39 +0530

    n=int(input()) a=list(map(int,input().split())) sum1=0 sum2=0 for i in range(len(a)):     if i%2==0:         sum1=sum1+a[i]     else:         sum2=sum2+a[i] if sum1>sum2:     sum=sum1-sum2 else:     sum=sum2-sum1 print(sum)

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_indexed_elements.py
@@ -0,0 +1,14 @@
+n=int(input())
+a=list(map(int,input().split()))
+sum1=0
+sum2=0
+for i in range(len(a)):
+    if i%2==0:
+        sum1=sum1+a[i]
+    else:
+        sum2=sum2+a[i]
+if sum1>sum2:
+    sum=sum1-sum2
+else:
+    sum=sum2-sum1
+print(sum)
