c894be4292bd06ffc0a5c02e7941714a68170283
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-06-02 21:06:30 +0530

    n=int(input()) a=list(map(int,input().split())) for i in range(len(a)):     if a[i]%2!=0:         print(a[i],end=' ') for i in range(len(a)):     if a[i]%2==0:         print(a[i],end=' ')

---
+++ array_of_add_and_even_elements.py
@@ -0,0 +1,8 @@
+n=int(input())
+a=list(map(int,input().split()))
+for i in range(len(a)):
+    if a[i]%2!=0:
+        print(a[i],end=' ')
+for i in range(len(a)):
+    if a[i]%2==0:
+        print(a[i],end=' ')
