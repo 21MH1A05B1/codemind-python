2a12babbea7ac9cb0dc1b358216b6bc197cdd500
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-06-03 21:51:30 +0530

    # elements less than avg lev 14 n=int(input()) a=list(map(int,input().split())) sum=0 c=0 for i in a:     sum=sum+i avg=(sum//n) for i in a:     if i<=avg:         c+=1 print(c)

---
+++ elements_smaller_than_average.py
@@ -0,0 +1,13 @@
+  # elements less than avg lev 14
+n=int(input())
+a=list(map(int,input().split()))
+sum=0
+c=0
+for i in a:
+    sum=sum+i
+avg=(sum//n)
+for i in a:
+    if i<=avg:
+        c+=1
+print(c)
+
