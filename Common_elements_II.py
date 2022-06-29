368cfe84724a658b15a306af7889a8161de169da
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-06-29 23:33:57 +0530

    n,m=map(int,input().split()) a=list(map(int,input().split())) b=list(map(int,input().split())) #a,b=set(a),set(b) #a,b=list(a),list(b) c=[] for i in range(len(a)):     if a[i] not in b:         c.append(a[i]) for i in range(len(b)):     if b[i] not in a:         c.append(b[i]) print(*c)

---
+++ Common_elements_II.py
@@ -0,0 +1,13 @@
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+#a,b=set(a),set(b)
+#a,b=list(a),list(b)
+c=[]
+for i in range(len(a)):
+    if a[i] not in b:
+        c.append(a[i])
+for i in range(len(b)):
+    if b[i] not in a:
+        c.append(b[i])
+print(*c)
