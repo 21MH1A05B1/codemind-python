00a77c0b1de28864de1f10f220df3c4960e73e95
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-06-29 14:25:33 +0530

    def hcf(a,b):     if a>b:         a,b=b,a     c=a     while True:         if a%c==0 and b%c==0:             return c         c-=1 a,b=map(int,input().split()) print(hcf(a,b))

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,11 +1,10 @@
-
-def hcf(a,b):#(3,9)
+def hcf(a,b):
     if a>b:
         a,b=b,a
-    c=a#c=3
+    c=a
     while True:
-        if a%c==0 and b%c==0:#3%3==0 and 6%3==0
-            return c #return 3
+        if a%c==0 and b%c==0:
+            return c
         c-=1
 a,b=map(int,input().split())
 print(hcf(a,b))
