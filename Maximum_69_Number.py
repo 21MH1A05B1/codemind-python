d553090d22d1942300b56253dd74581660227dac
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-06-29 21:03:53 +0530

    n=int(input()) t=n rev=0 c=0 ans=0 while t:     r=t%10     rev=rev*10+r     t=t//10 while rev:     r=rev%10     if r==6 and c==0:         r=9         c+=1     ans=(ans*10)+r     rev=rev//10 print(ans)

---
+++ Maximum_69_Number.py
@@ -0,0 +1,17 @@
+n=int(input())
+t=n
+rev=0
+c=0
+ans=0
+while t:
+    r=t%10
+    rev=rev*10+r
+    t=t//10
+while rev:
+    r=rev%10
+    if r==6 and c==0:
+        r=9
+        c+=1
+    ans=(ans*10)+r
+    rev=rev//10
+print(ans)
