bff41addd37fd87f2be4732ee00b48360bc81f47
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-01 20:51:12 +0530

    s=list(map(str,input().split())) c=0 for i in s:     for j in i:         if ord(j)>=97 and ord(j)<=122:             c+=1 print(c)

--- lowercase_letters_in_a_string.py
+++ lowercase_letters_in_a_string.py
@@ -1,6 +1,7 @@
-a=input()
+s=list(map(str,input().split()))
 c=0
-for i in a:
-    if(ord(i)>=97 and ord(i)<=122):
-        c+=1
+for i in s:
+    for j in i:
+        if ord(j)>=97 and ord(j)<=122:
+            c+=1
 print(c)
