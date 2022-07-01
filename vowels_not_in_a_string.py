a6a9d863033fa3eaafa5a24f5340e9b81ba57eb6
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-01 18:42:06 +0530

    n=input() n=n.lower() s="" s1='aeiou' c=0 for i in n:     if i in s1:         s+=i for i in s1:     if i not in s:         print(i,end=' ')         c=1 if c!=1:     print('0')

---
+++ vowels_not_in_a_string.py
@@ -0,0 +1,14 @@
+n=input()
+n=n.lower()
+s=""
+s1='aeiou'
+c=0
+for i in n:
+    if i in s1:
+        s+=i
+for i in s1:
+    if i not in s:
+        print(i,end=' ')
+        c=1
+if c!=1:
+    print('0')
