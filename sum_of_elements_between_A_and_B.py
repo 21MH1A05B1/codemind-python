d72d3aba74032b8c9e49b24573e1cfca3851e3b3
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-04 18:46:13 +0530

    n=int(input()) x=list(map(int,input().split())) a,b=map(int,(input().split())) s=0 for i in x:     if i>=a and i<=b:         s=s+i print(s)

--- sum_of_elements_between_A_and_B.py
+++ sum_of_elements_between_A_and_B.py
@@ -1,8 +1,9 @@
 n=int(input())
-a=list(map(int,input().split()))
-b,c=map(int,input().split())
+x=list(map(int,input().split()))
+a,b=map(int,(input().split()))
 s=0
-for i in range(n):
-    if a[i]>=b and a[i]<=c:
-        s+=a[i]
+for i in x:
+    if i>=a and i<=b:
+        s=s+i
 print(s)
+        
