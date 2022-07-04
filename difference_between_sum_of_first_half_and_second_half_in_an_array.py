678bff7b22e54db1687332393b2e13769413f1a3
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-04 22:10:03 +0530

    n=int(input()) a=list(map(int,input().split())) s1=s2=0 for i in range(1,(n//2)+1):     s1=s1+i for i in range((n//2)+1,n+1):     s2=s2+i print(abs(s2-s1))

--- difference_between_sum_of_first_half_and_second_half_in_an_array.py
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.py
@@ -1,9 +1,8 @@
 n=int(input())
 a=list(map(int,input().split()))
-s1=0
-s2=0
-for i in range(0,(n//2)+1):
+s1=s2=0
+for i in range(1,(n//2)+1):
     s1=s1+i
 for i in range((n//2)+1,n+1):
     s2=s2+i
-print(abs(s1-s2))
+print(abs(s2-s1))
