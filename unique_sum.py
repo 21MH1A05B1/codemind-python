53c5727dcb81d461f28a3cf2b4ec35546cd9b37b
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-04 21:36:18 +0530

    n=int(input()) a=list(map(int,input().split())) e=[] s=0 for i in a:     if i not in e:         e.append(i) for i in e:     s+=i print(s)

--- unique_sum.py
+++ unique_sum.py
@@ -1,11 +1,10 @@
 n=int(input())
 a=list(map(int,input().split()))
-s=[]
+e=[]
+s=0
 for i in a:
-    if i not in s:
-        s.append(i)
-x=0
-for i in s:
-    x+=i
-print(x)
-        
+    if i not in e:
+        e.append(i)
+for i in e:
+    s+=i
+print(s)
