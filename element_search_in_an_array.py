ad0e5e967bde2f805d190665100bc3e4a527ae0f
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-05 14:20:13 +0530

    n=int(input()) a=list(map(int,input().split())) b=int(input()) for i in range(n):     if a[i]==b:         print('True')         break else:     print('False')

--- element_search_in_an_array.py
+++ element_search_in_an_array.py
@@ -1,11 +1,9 @@
 n=int(input())
 a=list(map(int,input().split()))
 b=int(input())
-for i in a:
-    if b in a:
+for i in range(n):
+    if a[i]==b:
         print('True')
         break
-    else:
-        print('False')
-        break
-    
+else:
+    print('False')
