f2c9294eb3953b8871fbb67e2f2b5fd1c21add88
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-09 21:28:25 +0530

    n=input() n=n.lower() rev=n[::-1] if n==rev:     print('True') else:     print('False')

--- Palindrome_or_not.py
+++ Palindrome_or_not.py
@@ -1,11 +1,7 @@
-def poli(n):
-    if n.lower()==n.lower()[::-1]:
-        return 1
 n=input()
-for i in n:
-    if poli(n):
-        print('True')
-        break
-    else:
-        print('False')
-        break
+n=n.lower()
+rev=n[::-1]
+if n==rev:
+    print('True')
+else:
+    print('False')
