0f30570cdc74dc12a6ed7e1be046c75951c0eac8
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-09 21:16:21 +0530

    def poli(n):     b=n[::-1]     if n==b:         return True     else:         return False n=input() n=n.lower() n=n.split() c=0 for i in n:     if poli(i):         c+=1 print(c)

--- Count_Palindrome_words_in_a_string.py
+++ Count_Palindrome_words_in_a_string.py
@@ -1,12 +1,14 @@
-def poli(s):
-    if s.lower()==s.lower()[::-1]:
-        return 1
-def count_poli(s):
-    c=0
-    s=s.split(' ')
-    for i in s:
-        if poli(i):
-            c+=1
-    print(c)
+def poli(n):
+    b=n[::-1]
+    if n==b:
+        return True
+    else:
+        return False
 n=input()
-count_poli(n)
+n=n.lower()
+n=n.split()
+c=0
+for i in n:
+    if poli(i):
+        c+=1
+print(c)
