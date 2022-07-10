5c647de21519f2e0a140532b2f8d506c0991308a
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-10 16:18:22 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() s1=s1.split() s2=s2.split() c=0 for i in s1:     if i!=' ':         if i in s2:             c+=1 print(c)

--- common_words_-1.py
+++ common_words_-1.py
@@ -1,14 +1,12 @@
 s1=input()
 s2=input()
-a=[]
 s1=s1.lower()
 s2=s2.lower()
 s1=s1.split()
 s2=s2.split()
 c=0
 for i in s1:
-    for j in s2:
-        if i==j:
+    if i!=' ':
+        if i in s2:
             c+=1
 print(c)
-    
