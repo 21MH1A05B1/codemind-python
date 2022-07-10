f6eda4c7313969949f3d633edb0f2df84a220ae7
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-10 16:12:41 +0530

    s1=input() s2=input() s1=s1.lower() s2=s2.lower() s1=set(s1) s2=set(s2) c=0 for i in s1:     if i!=' ':         if i in s2:             c+=1 for i in s2:     if i!=' ':         if i in s1:             c+=1 print(c//2)

---
+++ common_characters_-II.py
@@ -0,0 +1,16 @@
+s1=input()
+s2=input()
+s1=s1.lower()
+s2=s2.lower()
+s1=set(s1)
+s2=set(s2)
+c=0
+for i in s1:
+    if i!=' ':
+        if i in s2:
+            c+=1
+for i in s2:
+    if i!=' ':
+        if i in s1:
+            c+=1
+print(c//2)
