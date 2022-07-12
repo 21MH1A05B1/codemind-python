d2a9b81640dae195b4c0d68a63cf26f5a1b38be6
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-12 20:30:36 +0530

    n=input() n=n.lower() n=n.replace(" ","") a=[] for i in n:     if ord(i)>=96 and ord(i)<=122:         if i not in a:             a.append(i) if len(a)==26:     print('True') else:     print('False')

--- Pangram_or_not.py
+++ Pangram_or_not.py
@@ -1,12 +1,13 @@
 n=input()
-s=n.lower()
-b=[]
-for i in s:
-    if ord(i)>=97 and ord(i)<=122:
-        if i not in b:
-            b.append(i)
-if len(b)==26:
+n=n.lower()
+n=n.replace(" ","")
+a=[]
+for i in n:
+    if ord(i)>=96 and ord(i)<=122:
+        if i not in a:
+            a.append(i)
+if len(a)==26:
     print('True')
 else:
     print('False')
-        
+    
