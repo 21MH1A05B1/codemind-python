4e34b482cee51ebd3e891ee1ca93abb76dda40ed
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-07-15 14:37:26 +0530

    a,b=map(int,input().split()) n=list(map(int,input().split())) d=[] for i in n:     if i>=0:         i=str(i)         x=len(i)         d.append(x)     else:         i=str(i)         x=len(i)         d.append(x-1) c=0 for i in d:     if i==b:         c+=1 print(c)

---
+++ K_digit_length_numbers_in_a_array.py
@@ -0,0 +1,20 @@
+a,b=map(int,input().split())
+n=list(map(int,input().split()))
+d=[]
+for i in n:
+    if i>=0:
+        i=str(i)
+        x=len(i)
+        d.append(x)
+    else:
+        i=str(i)
+        x=len(i)
+        d.append(x-1)
+c=0
+for i in d:
+    if i==b:
+        c+=1
+print(c)
+        
+        
+        
