2cf7de3dea0d73067ca065e834ed7c19bcfcdfba
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-09-20 21:51:57 +0530

    n=int(input()) x=list(map(int,input().split())) d=[] c=0 for i in x:     if i==x.count(i):         if i not in d:             d.append(i)             c+=1 if c==0:     print('-1') else:     print(*d)

--- Elements_same_as_count.py
+++ Elements_same_as_count.py
@@ -1,13 +1,13 @@
 n=int(input())
 x=list(map(int,input().split()))
-a=[]
+d=[]
 c=0
 for i in x:
     if i==x.count(i):
-        if i not in a:
-            a.append(i)
+        if i not in d:
+            d.append(i)
             c+=1
 if c==0:
     print('-1')
 else:
-    print(*a)
+    print(*d)
