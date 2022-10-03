9e3cf58c4663436852cc2cf855b1e44c04aca9ec
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-10-03 22:21:44 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in range(1,n-1,2):
        if a[i-1]<a[i] and a[i]>a[i-1]:
                c+=1
        else:
            print('-1')
            c=0
            break
    if c!=0:
        print(c)

--- wave_count.py
+++ wave_count.py
@@ -2,12 +2,12 @@ n=int(input())
 a=list(map(int,input().split()))
 c=0
 for i in range(1,n-1,2):
-    if a[i]>a[i-1] and a[i-1]<a[i]:
-        c+=1
+    
+    if a[i-1]<a[i] and a[i]>a[i-1]:
+            c+=1
     else:
         print('-1')
         c=0
         break
-       
 if c!=0:
     print(c)
