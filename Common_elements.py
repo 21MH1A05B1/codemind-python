31e09d0fac722e2a35a3267f313f241e7c2e6eed
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-10-03 13:54:47 +0530

    a,b=map(int,input().split()) x=list(map(int,input().split())) y=list(map(int,input().split())) s=[] s2=[] l=[] for i in x:     if i not in s:         s.append(i) for j in set(y):     if j not in s2:         s2.append(j) for i in s:     for j in s2:         if i==j:             l.append(i) print(*l)

--- Common_elements.py
+++ Common_elements.py
@@ -1,12 +1,18 @@
 a,b=map(int,input().split())
 x=list(map(int,input().split()))
 y=list(map(int,input().split()))
-e=[]
+s=[]
+s2=[]
+l=[]
 for i in x:
-    if i in y and i not in e:
-        e.append(i)
-print(*e,end=' ')
-    
-
-    
-        
+    if i not in s:
+        s.append(i)
+for j in set(y):
+    if j not in s2:
+        s2.append(j)
+for i in s:
+    for j in s2:
+        if i==j:
+            l.append(i)
+print(*l)
+            
