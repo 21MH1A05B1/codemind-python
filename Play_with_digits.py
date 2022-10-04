ea2d041ac546151ec2e75b5cc603df443b295ee8
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-10-04 19:30:56 +0530

    def dig(n):     s=0     while n:         s+=n%10         n//=10     return s n=int(input()) a=list(map(int,input().split())) s2=0 for i in a:     s2+=dig(i) print(s2)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,13 +1,12 @@
-def ds(n):
+def dig(n):
     s=0
     while n:
-        r=n%10
-        s+=r
+        s+=n%10
         n//=10
     return s
 n=int(input())
 a=list(map(int,input().split()))
-s1=0
+s2=0
 for i in a:
-    s1+=ds(i)
-print(s1)
+    s2+=dig(i)
+print(s2)
