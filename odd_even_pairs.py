2fcdb6432e1d33f6802d37cbba2111b64481b23c
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-10-04 22:04:43 +0530

    n = int(input()) arr = list(map(int,input().split())) e = [] o = [] for i in range(0,n):     if arr[i]%2==0:         e.append(arr[i])     else:         o.append(arr[i]) i = 0 j = 0 while i<len(e) or j<len(o):     if j<len(o):         print(o[j],end=" ")         j+=1     if i<len(e):         print(e[i],end=" ")         i+=1 if n%2!=0:     print("0")

--- odd_even_pairs.py
+++ odd_even_pairs.py
@@ -1,20 +1,20 @@
-n=int(input())
-a=list(map(int,input().split()))
-e=[]
-o=[]
+n = int(input())
+arr = list(map(int,input().split()))
+e = []
+o = []
 for i in range(0,n):
-    if a[i]%2!=0:
-        o.append(a[i])
+    if arr[i]%2==0:
+        e.append(arr[i])
     else:
-        e.append(a[i])
-i=0
-j=0
-while i<len(o) or j<len(e):
-    if i<len(o):
-        print(o[i],end=' ')
-        i+=1
-    if j<len(e):
-        print(e[j],end=' ')
+        o.append(arr[i])
+i = 0
+j = 0
+while i<len(e) or j<len(o):
+    if j<len(o):
+        print(o[j],end=" ")
         j+=1
+    if i<len(e):
+        print(e[i],end=" ")
+        i+=1
 if n%2!=0:
-    print('0')
+    print("0")
