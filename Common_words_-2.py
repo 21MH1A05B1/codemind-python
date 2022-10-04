3c5a17d0afa26efd7476ffd39c24610b9e60941e
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-10-04 19:57:40 +0530

    s1=input().split() s2=input().split() c=0 for i in s1 and s2:     if s1.count(i)==1 and s2.count(i)==1:         if i in s2:             c+=1 print(c)

--- Common_words_-2.py
+++ Common_words_-2.py
@@ -1,18 +1,8 @@
-s1=input()
-s2=input()
-s1=s1.split()
-s2=s2.split()
-a=[]
-b=[]
+s1=input().split()
+s2=input().split()
 c=0
-for i in s1:
-    if s1.count(i)==1:
-        a.append(i)
-for i in s2:
-    if s2.count(i)==1:
-        b.append(i)
-for i in a:
-    if i in b:
-        c+=1
+for i in s1 and s2:
+    if s1.count(i)==1 and s2.count(i)==1:
+        if i in s2:
+            c+=1
 print(c)
-        
