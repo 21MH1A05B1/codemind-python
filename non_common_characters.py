e94646480c0a1365ac3de9962ec42686115fd437
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-10-07 12:32:54 +0530

    n=input() n1=input() n=n.lower() n1=n1.lower() n=n.replace(" ","") n1=n1.replace(" ","") l=[] for i in n:     if i not in n1:         l.append(i) for j in n1:     if j not in n:         l.append(j) for k in sorted(set(l)):     print(k,end='')

--- non_common_characters.py
+++ non_common_characters.py
@@ -1,14 +1,15 @@
-s1=input().lower()
-s2=input().lower()
-s3=[]
-for i in s1:
-    if i not in s2:
-        s3.append(i)
-for i in s2:
-    if i not in s1:
-        s3.append(i)
-b=set(s3)
-c=sorted(b)
-for i in c:
-    if i!=" ":
-        print(i,end='')
+n=input()
+n1=input()
+n=n.lower()
+n1=n1.lower()
+n=n.replace(" ","")
+n1=n1.replace(" ","")
+l=[]
+for i in n:
+    if i not in n1:
+        l.append(i)
+for j in n1:
+    if j not in n:
+        l.append(j)
+for k in sorted(set(l)):
+    print(k,end='')
