4ffd78a6a3585dc9b1d607792c1b5d535bcd76ba
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-10-07 12:06:40 +0530

    s=input().replace(" ","").lower() s1='' c=0 s1=set(s) s1=sorted(s1) for i in s1:     if i!=" ":         c+=1 print(c)

--- Count_Distinct_characters.py
+++ Count_Distinct_characters.py
@@ -1,5 +1,9 @@
-s=input()
-s=s.lower()
-s=s.replace(" ","")
-s2=set(s)
-print(len(s2))
+s=input().replace(" ","").lower()
+s1=''
+c=0
+s1=set(s)
+s1=sorted(s1)
+for i in s1:
+    if i!=" ":
+        c+=1
+print(c)
