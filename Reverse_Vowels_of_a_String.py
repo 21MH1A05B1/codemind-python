eef28454ef326197da168baa76ee25dcc51a7163
21MH1A05B1 <97426896+21MH1A05B1@users.noreply.github.com>
2022-12-23 09:48:11 +0530

    n=input() c=[] k=0 for i in n:     if i in "aeiouAEIOU":         c.append(i) c=c[::-1] for i in n:     if i not in "aeiouAEIOU":         print(i,end="")     else:         print(c[k],end="")         k+=1

---
+++ Reverse_Vowels_of_a_String.py
@@ -0,0 +1,14 @@
+
+n=input()
+c=[]
+k=0
+for i in n:
+    if i in "aeiouAEIOU":
+        c.append(i)
+c=c[::-1]
+for i in n:
+    if i not in "aeiouAEIOU":
+        print(i,end="")
+    else:
+        print(c[k],end="")
+        k+=1
